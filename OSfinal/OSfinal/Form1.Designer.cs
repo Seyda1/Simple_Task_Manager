3c1afd3a28d1f2f3ca6cdac410b9f7620f743596
Şeyda Aybar <seydaybar@gmail.com>
2021-02-24 00:08:32 +0300

    başarılı

---
+++ OSfinal/OSfinal/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace OSfinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Proseslstbx = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.kullandığıBellekMiktarıToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kullandığıİşlemciYüzdesiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.prosesiÖldürToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Proseslstbx
+            // 
+            this.Proseslstbx.ContextMenuStrip = this.contextMenuStrip1;
+            this.Proseslstbx.FormattingEnabled = true;
+            this.Proseslstbx.Location = new System.Drawing.Point(12, 12);
+            this.Proseslstbx.Name = "Proseslstbx";
+            this.Proseslstbx.Size = new System.Drawing.Size(422, 407);
+            this.Proseslstbx.TabIndex = 0;
+            this.Proseslstbx.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Proseslstbx_MouseDown);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.kullandığıBellekMiktarıToolStripMenuItem,
+            this.kullandığıİşlemciYüzdesiToolStripMenuItem,
+            this.prosesiÖldürToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(210, 92);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(209, 22);
+            this.toolStripMenuItem1.Text = "Prosesin thread sayısı";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // kullandığıBellekMiktarıToolStripMenuItem
+            // 
+            this.kullandığıBellekMiktarıToolStripMenuItem.Name = "kullandığıBellekMiktarıToolStripMenuItem";
+            this.kullandığıBellekMiktarıToolStripMenuItem.Size = new System.Drawing.Size(209, 22);
+            this.kullandığıBellekMiktarıToolStripMenuItem.Text = "Kullandığı bellek miktarı";
+            this.kullandığıBellekMiktarıToolStripMenuItem.Click += new System.EventHandler(this.kullandığıBellekMiktarıToolStripMenuItem_Click);
+            // 
+            // kullandığıİşlemciYüzdesiToolStripMenuItem
+            // 
+            this.kullandığıİşlemciYüzdesiToolStripMenuItem.Name = "kullandığıİşlemciYüzdesiToolStripMenuItem";
+            this.kullandığıİşlemciYüzdesiToolStripMenuItem.Size = new System.Drawing.Size(209, 22);
+            this.kullandığıİşlemciYüzdesiToolStripMenuItem.Text = "Kullandığı işlemci yüzdesi";
+            this.kullandığıİşlemciYüzdesiToolStripMenuItem.Click += new System.EventHandler(this.kullandığıİşlemciYüzdesiToolStripMenuItem_Click);
+            // 
+            // prosesiÖldürToolStripMenuItem
+            // 
+            this.prosesiÖldürToolStripMenuItem.Name = "prosesiÖldürToolStripMenuItem";
+            this.prosesiÖldürToolStripMenuItem.Size = new System.Drawing.Size(209, 22);
+            this.prosesiÖldürToolStripMenuItem.Text = "Prosesi öldür";
+            this.prosesiÖldürToolStripMenuItem.Click += new System.EventHandler(this.prosesiÖldürToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(489, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Coral;
+            this.ClientSize = new System.Drawing.Size(448, 525);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Proseslstbx);
+            this.Name = "Form1";
+            this.Text = "Görev Yöneticisi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox Proseslstbx;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem kullandığıBellekMiktarıToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kullandığıİşlemciYüzdesiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem prosesiÖldürToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ OSfinal/OSfinal/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+namespace OSfinal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ProsesGüncelleme();
+            
+        }
+        public void ProsesGüncelleme()
+        {
+            Proseslstbx.Items.Clear();
+            foreach (Process proses in Process.GetProcesses())
+            {
+                Proseslstbx.Items.Add(proses.ProcessName);
+            }
+        }
+        public void ProsesÖldürme()
+        {
+            Process[] proses = Process.GetProcessesByName(Proseslstbx.SelectedItem.ToString());
+            foreach (Process pro in proses)
+            {
+                pro.Kill();
+               
+            }
+            MessageBox.Show("Öldürülen proses : "+proses[0]);
+            ProsesGüncelleme();
+        }
+        
+        public void KullanılanİşlemciYüzdesi()
+        {
+            String Prosesisim = Proseslstbx.SelectedItem.ToString();
+            PerformanceCounter İşlemciSayaç = new PerformanceCounter("Process", "% Processor Time", Prosesisim);
+            İşlemciSayaç.NextValue();
+
+            Thread.Sleep(1000);
+
+            float yüzde = İşlemciSayaç.NextValue() / Environment.ProcessorCount;
+            String yüzdeSon=yüzde.ToString();
+            MessageBox.Show(Prosesisim+" prosesinin kullandığı işlemci yüzdesi : "+yüzdeSon);
+        }
+        public void BellekKullanımı()
+        {
+            String Prosesisim = Proseslstbx.SelectedItem.ToString();
+            PerformanceCounter BellekSayaç = new PerformanceCounter("Process", "Working Set - Private", Prosesisim,true);
+
+            double BellekKullanımı = Convert.ToDouble(BellekSayaç.NextValue()) / (1024 * 1024);
+
+            String BellekKullanımıSon = BellekKullanımı.ToString();
+            MessageBox.Show(Prosesisim + " prosesinin kullandığı bellek miktarı : " + BellekKullanımıSon +" MB ");
+            BellekSayaç.Close();
+            BellekSayaç.Dispose();
+          
+           
+            
+        }
+        public void ThreadSayısı()
+        {
+            Process[] proses = Process.GetProcessesByName(Proseslstbx.SelectedItem.ToString());
+            double threadSayısı = Convert.ToDouble(proses[0].Threads.Count);
+            
+            MessageBox.Show(proses[0]+"prosesinin thread sayısı : "+threadSayısı.ToString());
+            
+        }
+
+        private void prosesiÖldürToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ProsesÖldürme();
+        }
+
+        private void Proseslstbx_MouseDown(object sender, MouseEventArgs e)
+        {
+            Proseslstbx.SelectedIndex = Proseslstbx.IndexFromPoint(e.X, e.Y);
+        }
+
+        private void kullandığıİşlemciYüzdesiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            KullanılanİşlemciYüzdesi();
+        }
+
+        private void kullandığıBellekMiktarıToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            BellekKullanımı();
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            ThreadSayısı();
+        }
+       
+    }
+}
